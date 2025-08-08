2858c23c83ec76efce13f91ee028167706765d5b
Sean Campbell <sean.campbell@impact.com>
2025-08-08 15:47:27 +0200

    DEVE-354: Refactored the GitGuardian API pagination logic to look for the next URL in the response header and not in the JSON response body

--- gitguardian/client.py
+++ gitguardian/client.py
@@ -1,5 +1,5 @@
 import httpx
-from httpx import URL
+from httpx import URL, Response
 from enum import StrEnum
 from typing import AsyncGenerator, Optional, Any
 from port_ocean.utils import http_async_client
@@ -42,7 +42,7 @@ class GitGuardianClient:
                 "Authorization": f"Token {self.api_key}",
                 "Content-Type": "application/json", 
             }
-        }
+        }  
     
     async def _send_api_request(
         self,
@@ -50,7 +50,7 @@ class GitGuardianClient:
         method: str = "GET",
         query_params: Optional[dict[str, Any]] = None,
         json_data: Optional[dict[str, Any]] = None,
-    ) -> dict[str, Any]:
+    ) -> Response:
         logger.debug(
             f"Sending API request to {method} {endpoint} with query params: {query_params}"
         )
@@ -62,7 +62,7 @@ class GitGuardianClient:
                 json=json_data,
             )
             response.raise_for_status()
-            return response.json()
+            return response
         except httpx.HTTPStatusError as e:
             logger.error(
                 f"HTTP error with status code: {e.response.status_code} and response text: {e.response.text}"
@@ -74,6 +74,21 @@ class GitGuardianClient:
                 return {}
             raise
 
+    async def _get_api_response_json(
+        self,
+        endpoint: str,
+        method: str = "GET",
+        query_params: Optional[dict[str, Any]] = None,
+        json_data: Optional[dict[str, Any]] = None,
+    ) -> dict[str, Any]:
+        response = await self._send_api_request(
+                    endpoint=endpoint,
+                    method=method,
+                    query_params={**(query_params or {}), "per_page": PAGE_SIZE},
+                    json_data=json_data
+                )
+        return response.json()
+
     async def _send_paginated_request(
         self,
         endpoint: str,
@@ -82,16 +97,17 @@ class GitGuardianClient:
     ) -> AsyncGenerator[list[Any], None]:
         while endpoint:
             try:
-                data = await self._send_api_request(
+                response = await self._send_api_request(
                     endpoint=endpoint,
                     method=method,
                     query_params={**(query_params or {}), "per_page": PAGE_SIZE},
                 )
 
+                data = response.json
                 yield data.get("data", [])
 
-                # Check if there is a "next" URL in the links object
-                next_url = data.get("links", {}).get("next", "url")
+                # Check if there is a "next" URL in the response headers
+                next_url = response.headers.get("link", {}).get("next", "url")
                 if next_url:
                     parsed_url = URL(next_url)
                     endpoint = parsed_url.raw_path.decode().replace(f"{self.base_url}/{self.api_version}", "")
@@ -103,67 +119,67 @@ class GitGuardianClient:
                 logger.error(
                     f"HTTP error with status code: {e.response.status_code} and response text: {e.response.text}"
                 )
-                raise    
+                raise     
 
     async def get_audit_logs(self) -> AsyncGenerator[list[dict[str, Any]], None]:
         logger.info(f"Fetching all audit logs from GitGuardian.")
-        async for audit_logs in self._send_api_request(endpoint=Endpoints.AUDIT_LOGS):
+        async for audit_logs in self._get_api_response_json(endpoint=Endpoints.AUDIT_LOGS):
             yield audit_logs
 
     async def get_custom_tags(self) -> AsyncGenerator[list[dict[str, Any]], None]:
         logger.info(f"Fetching all existing custom tags from GitGuardian.")
-        async for custom_tags in self._send_api_request(endpoint=Endpoints.CUSTOM_TAGS):
+        async for custom_tags in self._get_api_response_json(endpoint=Endpoints.CUSTOM_TAGS):
             yield custom_tags
 
     async def get_developers(self) -> AsyncGenerator[list[dict[str, Any]], None]:
         logger.info(f"Fetching developers in the public perimeter from GitGuardian.")
-        async for developers in self._send_api_request(endpoint=Endpoints.DEVELOPERS):
+        async for developers in self._get_api_response_json(endpoint=Endpoints.DEVELOPERS):
             yield developers
 
     async def get_health(self) -> dict[str, Any]:        
         logger.info(f"Checking the status of the API and token.")
-        return await self._send_api_request(endpoint=Endpoints.HEALTH)
+        return await self._get_api_response_json(endpoint=Endpoints.HEALTH)
     
     async def get_internal_secret_incidents(self) -> AsyncGenerator[list[dict[str, Any]], None]:
         logger.info(f"Fetching secret incidents detected by the GitGuardian dashboard.")
-        async for incidents in self._send_api_request(endpoint=Endpoints.INTERNAL_SECRET_INCIDENTS):
+        async for incidents in self._get_api_response_json(endpoint=Endpoints.INTERNAL_SECRET_INCIDENTS):
             yield incidents
 
     async def get_single_internal_secret_incidents(self, incident_id: int) -> dict[str, Any]:   
         logger.info(f"Fetching specific secret incidents detected by the GitGuardian dashboard.")
-        return await self._send_api_request(endpoint=f"{Endpoints.INTERNAL_SECRET_INCIDENTS}/{incident_id}")
+        return await self._get_api_response_json(endpoint=f"{Endpoints.INTERNAL_SECRET_INCIDENTS}/{incident_id}")
 
     async def get_internal_secret_occurrences(self) -> AsyncGenerator[list[dict[str, Any]], None]:
         logger.info(f"Fetching occurrences of secrets in the monitored perimeter.")
-        async for occurrences in self._send_api_request(endpoint=Endpoints.INTERNAL_SECRET_OCCURRENCES):
+        async for occurrences in self._get_api_response_json(endpoint=Endpoints.INTERNAL_SECRET_OCCURRENCES):
             yield occurrences
 
     async def get_workspace_members(self) -> AsyncGenerator[list[dict[str, Any]], None]:
         logger.info(f"Fetching all members of the GitGuardian workspace.")
-        async for members in self._send_api_request(endpoint=Endpoints.WORKSPACE_MEMBERS):
+        async for members in self._get_api_response_json(endpoint=Endpoints.WORKSPACE_MEMBERS):
             yield members
 
     async def get_single_workspace_member(self, member_id: int) -> dict[str, Any]:   
         logger.info(f"Fetching specific GitGuardian workspace member.")
-        return await self._send_api_request(endpoint=f"{Endpoints.WORKSPACE_MEMBERS}/{member_id}")
+        return await self._get_api_response_json(endpoint=f"{Endpoints.WORKSPACE_MEMBERS}/{member_id}")
     
     async def get_member_teams(self, member_id: int) -> AsyncGenerator[list[dict[str, Any]], None]:
         logger.info(f"Fetching all members of the GitGuardian workspace.")
-        async for member_teams in self._send_api_request(endpoint=f"{Endpoints.WORKSPACE_MEMBERS}/{member_id}/teams"):
+        async for member_teams in self._get_api_response_json(endpoint=f"{Endpoints.WORKSPACE_MEMBERS}/{member_id}/teams"):
             yield member_teams
 
     async def get_public_secret_incidents(self) -> AsyncGenerator[list[dict[str, Any]], None]:
         logger.info(f"Fetching public secret incidents detected by the GitGuardian dashboard.")
-        async for public_incidents in self._send_api_request(endpoint=Endpoints.PUBLIC_SECRET_INCIDENTS):
+        async for public_incidents in self._get_api_response_json(endpoint=Endpoints.PUBLIC_SECRET_INCIDENTS):
             yield public_incidents
 
     async def get_single_public_secret_incidents(self, incident_id: int) -> dict[str, Any]:   
         logger.info(f"Fetching specific public secret incidents detected by the GitGuardian dashboard.")
-        return await self._send_api_request(endpoint=f"{Endpoints.PUBLIC_SECRET_INCIDENTS}/{incident_id}")
+        return await self._get_api_response_json(endpoint=f"{Endpoints.PUBLIC_SECRET_INCIDENTS}/{incident_id}")
     
     async def get_public_secret_occurrences(self, incident_id: int) -> AsyncGenerator[list[dict[str, Any]], None]:
         logger.info(f"Fetching public secret occurrences detected by the GitGuardian dashboard.")
-        async for public_occurrences in self._send_api_request(endpoint=f"{Endpoints.PUBLIC_SECRET_INCIDENTS}/{incident_id}/occurrences"):
+        async for public_occurrences in self._get_api_response_json(endpoint=f"{Endpoints.PUBLIC_SECRET_INCIDENTS}/{incident_id}/occurrences"):
             yield public_occurrences
 
     async def get_sources(self) -> AsyncGenerator[list[dict[str, Any]], None]:
@@ -173,7 +189,7 @@ class GitGuardianClient:
 
     async def get_single_source(self, source_id: int) -> dict[str, Any]:   
         logger.info(f"Fetching a single source known by GitGuardian.")
-        return await self._send_api_request(endpoint=f"{Endpoints.SOURCES}/{source_id}")
+        return await self._get_api_response_json(endpoint=f"{Endpoints.SOURCES}/{source_id}")
 
     async def get_sources_secret_incidents(self, source_id: int) -> AsyncGenerator[list[dict[str, Any]], None]:
         logger.info(f"Fetching secret incidents linked to a source.")
@@ -192,7 +208,7 @@ class GitGuardianClient:
 
     async def get_single_team(self, team_id: int) -> dict[str, Any]:   
         logger.info(f"Fetching a single source known by GitGuardian.")
-        return await self._send_api_request(endpoint=f"{Endpoints.TEAMS}/{team_id}")
+        return await self._get_api_response_json(endpoint=f"{Endpoints.TEAMS}/{team_id}")
     
     async def get_team_secrets(self, team_id: int) -> AsyncGenerator[list[dict[str, Any]], None]:
         logger.info(f"Fetching all teams in GitGuardian workspace.")
--- tests/test_client.py
+++ tests/test_client.py
@@ -3,8 +3,7 @@ import pytest
 from unittest.mock import AsyncMock, MagicMock, patch
 from port_ocean.context.ocean import initialize_port_ocean_context
 from port_ocean.exceptions.context import PortOceanContextAlreadyInitializedError
-from gitguardian.client import Endpoints, GitGuardianClient
-from port_ocean.utils import http_async_client
+from gitguardian.client import PAGE_SIZE, Endpoints, GitGuardianClient
 from httpx import HTTPStatusError, Request, Response
 
 
@@ -54,7 +53,7 @@ async def test_send_api_request_success(mock_gitguardian_client: GitGuardianClie
         mock_request.return_value = Response(
             200, request=Request("GET", "/some_endpoint"), json={"key": "value"}
         )
-        response = await mock_gitguardian_client._send_api_request("GET", "/some_endpoint")
+        response = await mock_gitguardian_client._get_api_response_json("GET", "/some_endpoint")
         assert response["key"] == "value"
 
 @pytest.mark.asyncio
@@ -76,7 +75,7 @@ async def test_get_single_source(mock_gitguardian_client: GitGuardianClient) -> 
     source_data: dict[str, Any] = get_single_mocked_source(source_id)
 
     with patch.object(
-        mock_gitguardian_client, "_send_api_request", new_callable=AsyncMock
+        mock_gitguardian_client, "_get_api_response_json", new_callable=AsyncMock
     ) as mock_request:
         mock_request.return_value = source_data
         result = await mock_gitguardian_client.get_single_source(source_id)
@@ -88,16 +87,48 @@ async def test_get_single_source(mock_gitguardian_client: GitGuardianClient) -> 
 async def test_get_single_team(mock_gitguardian_client: GitGuardianClient) -> None:
     """Test get_single_team method"""
     team_id = 1313
-    team_data: dict[str, Any] = get_single_mocked_team(team_id)
+    team_data: dict[str, Any] = get_single_mocked_team(team_id, "feature team A")
 
     with patch.object(
-        mock_gitguardian_client, "_send_api_request", new_callable=AsyncMock
+        mock_gitguardian_client, "_get_api_response_json", new_callable=AsyncMock
     ) as mock_request:
         mock_request.return_value = team_data
         result = await mock_gitguardian_client.get_single_team(team_id)
 
         mock_request.assert_called_once_with(endpoint=f"{Endpoints.TEAMS}/{team_id}")
         assert result == team_data
+
+# @pytest.mark.asyncio
+# async def test_get_paginated_teams(mock_gitguardian_client: GitGuardianClient) -> None:
+#     """Test get_paginated_teams method"""
+#     team_data: dict[str, Any] = [f"{get_single_mocked_team(1234, "feature team A")}", f"{get_single_mocked_team(5678, "feature team B")}"]
+    
+#     with patch.object(
+#         mock_gitguardian_client, "_send_paginated_request", new_callable=AsyncMock
+#     ) as mock_request:
+#         mock_request.side_effect = [
+#             team_data,
+#             [],  # Empty response to end pagination
+#         ]
+#         mock_request.return_value = Response(
+#             200, 
+#             headers={"link": f"<https://mockapi.gitguardian.com/v1/teams?cursor=cD0yMDM5NjE4MQ%3D%3D&per_page={PAGE_SIZE}>; rel='next'"},
+#             json=team_data
+#         )
+
+#         mock_request.return_value = team_data
+
+#         teams: list[dict[str, Any]] = []
+#         async for team_batch in await mock_gitguardian_client.get_teams():
+#             teams.extend(team_batch)
+
+#         assert len(teams) == 2
+#         assert teams == team_data
+#         mock_request.assert_called_with(
+#             "GET",
+#             endpoint=f"{Endpoints.TEAMS}",
+#             params={"per_page": PAGE_SIZE},
+#         )
 
 @pytest.mark.asyncio
 async def test_get_single_workspace_member(mock_gitguardian_client: GitGuardianClient) -> None:
@@ -106,7 +137,7 @@ async def test_get_single_workspace_member(mock_gitguardian_client: GitGuardianC
     workspace_member_data: dict[str, Any] = get_single_mocked_workspace_member(member_id)
 
     with patch.object(
-        mock_gitguardian_client, "_send_api_request", new_callable=AsyncMock
+        mock_gitguardian_client, "_get_api_response_json", new_callable=AsyncMock
     ) as mock_request:
         mock_request.return_value = workspace_member_data
         result = await mock_gitguardian_client.get_single_workspace_member(member_id)
@@ -121,7 +152,7 @@ async def test_get_single_internal_secret_incident(mock_gitguardian_client: GitG
     secret_incident_data: dict[str, Any] = get_single_mocked_internal_secret_incident(incident_id)
 
     with patch.object(
-        mock_gitguardian_client, "_send_api_request", new_callable=AsyncMock
+        mock_gitguardian_client, "_get_api_response_json", new_callable=AsyncMock
     ) as mock_request:
         mock_request.return_value = secret_incident_data
         result = await mock_gitguardian_client.get_single_internal_secret_incidents(incident_id)
@@ -136,7 +167,7 @@ async def test_get_single_public_secret_incident(mock_gitguardian_client: GitGua
     public_secret_incident_data: dict[str, Any] = get_single_mocked_public_secret_incident(incident_id)
 
     with patch.object(
-        mock_gitguardian_client, "_send_api_request", new_callable=AsyncMock
+        mock_gitguardian_client, "_get_api_response_json", new_callable=AsyncMock
     ) as mock_request:
         mock_request.return_value = public_secret_incident_data
         result = await mock_gitguardian_client.get_single_public_secret_incidents(incident_id)
@@ -144,10 +175,10 @@ async def test_get_single_public_secret_incident(mock_gitguardian_client: GitGua
         mock_request.assert_called_once_with(endpoint=f"{Endpoints.PUBLIC_SECRET_INCIDENTS}/{incident_id}")
         assert result == public_secret_incident_data
 
-def get_single_mocked_team(team_id: int):
+def get_single_mocked_team(team_id: int, team_name: str):
     return {
         "id": team_id,
-        "name": "feature team A",
+        "name": team_name,
         "description": "Description of my team",
         "is_global": False,
         "gitguardian_url": "https://dashboard.gitguardian.com/workspace/1/settings/user/teams/1"
