04b0466b0334d9399a9610b67bb37f7dffcfca55
Sean Campbell <sean.campbell@impact.com>
2025-08-11 16:11:22 +0200

    DEVE-358: Implemented algorithm in base_webhook_processor to verify the webhook payload signature and so determine if the event should be processed

--- webhook_processors/base_webhook_processor.py
+++ webhook_processors/base_webhook_processor.py
@@ -1,21 +1,44 @@
+import hashlib
+import hmac
 from typing import Any
+from loguru import logger
+from port_ocean.context.ocean import ocean
 from port_ocean.core.handlers.webhook.abstract_webhook_processor import (
     AbstractWebhookProcessor,
 )
-from port_ocean.core.handlers.webhook.webhook_event import (
-    EventPayload,
-    WebhookEvent,
-)
+from port_ocean.core.handlers.webhook.webhook_event import EventPayload, WebhookEvent
 
 
 class BaseGitGuardianWebhookProcessor(AbstractWebhookProcessor):
     async def should_process_event(self, event: WebhookEvent) -> bool:
-        return True  # TODO: Improve implementation
+        if event._original_request is None:
+            return False
+
+        # See https://docs.gitguardian.com/platform/configure-alerting/notifiers-integrations/custom-webhook
+        webhook_secret = ocean.integration_config.get("webhook_secret")
+        signature = event.headers.get("gitguardian-signature", "")
+        timestamp = event.headers.get("timestamp", "")
+
+        if signature and not webhook_secret:
+            logger.warning(
+                "Signature found but no secret configured for authenticating incoming webhooks, skipping event."
+            )
+            return False
+
+        # Verify signature if webhook secret configured
+        body = await event._original_request.body()
+        computed_signature = hmac.new(
+            (timestamp + webhook_secret).encode("utf-8"),
+            body,
+            hashlib.sha256,
+        ).hexdigest()
+
+        return hmac.compare_digest(signature, computed_signature)
 
     async def authenticate(
         self, payload: EventPayload, headers: dict[str, Any]
     ) -> bool:
-        return True  # TODO: Improve this, don't just pass Authentication
+        return True
 
     async def validate_payload(self, payload: EventPayload) -> bool:
-        return True  # TODO: Improve validation, example: "project" in payload
+        return payload.get("source", "").lower() == "gitguardian"
