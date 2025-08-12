99156f395de5598a5ca97ee2cb6b6c9cff389e3b
Sean Campbell <sean.campbell@impact.com>
2025-08-12 10:24:57 +0200

    DEVE-358: Completed the implementation of the PublicSecretIncidentWebhookProcessor

--- webhook_processors/public_incident_webhook_processor.py
+++ webhook_processors/public_incident_webhook_processor.py
@@ -1,5 +1,4 @@
-from typing import cast
-from gitguardian.overrides import GitGuardianPublicSecretIncidentConfig
+from loguru import logger
 from initialize_client import init_gitguardian_client
 from port_ocean.core.handlers.port_app_config.models import ResourceConfig
 from port_ocean.core.handlers.webhook.webhook_event import (
@@ -19,7 +18,42 @@ class PublicSecretIncidentWebhookProcessor(BaseGitGuardianWebhookProcessor):
     async def handle_event(
         self, payload: EventPayload, resource_config: ResourceConfig
     ) -> WebhookEventRawResults:
-        client = init_gitguardian_client()
-        config = cast(GitGuardianPublicSecretIncidentConfig, resource_config)
+        webhook_event_type = payload.get("payload", {}).get("action")
 
-        # TODO: Add logic for handling event
+        if not webhook_event_type.endswith("_publicly"):
+            return self._empty_response(
+                f"Attempted to handle incorrect webhook event type: {webhook_event_type}"
+            )
+
+        incident_id = payload.get("payload", {}).get("incident", {}).get("id")
+
+        if incident_id is None:
+            return self._empty_response(
+                "Failed to retrieve an incident ID from the payload."
+            )
+
+        logger.debug(f"Fetching public incident with ID: {incident_id}")
+        client = await init_gitguardian_client()
+        incident_data = await client.get_single_public_secret_incidents(incident_id)
+
+        if incident_data is None:
+            return self._empty_response(
+                f"Failed to retrieve a public incident with ID: {incident_id}"
+            )
+
+        data_to_update = []
+        data_to_delete = []
+        logger.info(f"Processing public incident: {incident_id}")
+
+        if webhook_event_type == "incident_unshared_publicly":
+            data_to_delete.extend([incident_data])
+        else:
+            data_to_update.extend([incident_data])
+
+        return WebhookEventRawResults(
+            updated_raw_results=data_to_update,
+            deleted_raw_results=data_to_delete,
+        )
+
+    async def validate_payload(self, payload: EventPayload) -> bool:
+        return payload.get("payload", {}).get("action", "").endswith("_publicly")
