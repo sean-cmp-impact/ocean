ea6ccef77dcc2d5b4fd4bcfa2fb5abb104899955
Sean Campbell <sean.campbell@impact.com>
2025-08-13 11:01:50 +0200

    DEVE-363: Introduced a TestBaseWebhookProcessor abstract class to polymorphically test concrete webhook processor functions

--- tests/test_processors.py
+++ tests/test_processors.py
@@ -5,9 +5,11 @@ import pytest
 import hashlib
 from typing import Any
 from integration import ObjectKind
+from abc import ABC, abstractmethod
 from unittest.mock import AsyncMock, MagicMock
 from port_ocean.context.ocean import PortOceanContext
 from port_ocean.core.handlers.webhook.webhook_event import WebhookEvent
+from webhook_processors.base_webhook_processor import BaseGitGuardianWebhookProcessor
 from webhook_processors.internal_incident_webhook_processor import (
     InternalSecretIncidentWebhookProcessor,
 )
@@ -23,6 +25,13 @@ TIMESTAMP = str(time.time())
 @pytest.fixture
 def mock_event() -> WebhookEvent:
     return WebhookEvent(trace_id="test", payload={}, headers={})
+
+
+@pytest.fixture
+def base_processor(
+    mock_event: WebhookEvent,
+) -> BaseGitGuardianWebhookProcessor:
+    return BaseGitGuardianWebhookProcessor(mock_event)
 
 
 @pytest.fixture
@@ -51,69 +60,165 @@ def mock_context(monkeypatch: Any) -> MagicMock:
     return mock_context
 
 
-@pytest.mark.asyncio
-async def test_should_process_event_valid_signature_and_timestamp(
-    internal_incident_processor: InternalSecretIncidentWebhookProcessor,
-    mock_context: PortOceanContext,
-) -> None:
-    set_integration_config(mock_context)
+class TestBaseWebhookProcessor(ABC):
+    """For base functionality only. Polomorphic tests originates from concrete classes"""
 
-    payload = {"action": "incident_triggered"}
-    body = json.dumps(payload).encode("utf-8")
-    signature = build_signature(body, "sha256")
-    event = init_event(body, signature)
+    # This line prevents pytest from collecting tests in this base class
+    __test__ = False
 
-    result = await internal_incident_processor.should_process_event(event)
-    assert result is True
+    async def test_should_process_event_valid_signature_and_timestamp(
+        self,
+        base_processor: BaseGitGuardianWebhookProcessor,
+        mock_context: PortOceanContext,
+    ) -> None:
+        self._set_integration_config(mock_context)
+
+        payload = {"action": "incident_triggered"}
+        body = json.dumps(payload).encode("utf-8")
+        signature = self._get_signature(body, "sha256")
+        event = self._init_event(body, signature)
+
+        result = await base_processor.should_process_event(event)
+        assert result is True
+
+    @pytest.mark.asyncio
+    async def test_should_process_event_invalid_signature(
+        self,
+        base_processor: BaseGitGuardianWebhookProcessor,
+        mock_context: PortOceanContext,
+    ) -> None:
+        self._set_integration_config(mock_context)
+
+        payload = {"action": "incident_triggered"}
+        body = json.dumps(payload).encode("utf-8")
+        signature = "sha256=invalidsignature"
+        event = self._init_event(body, signature)
+
+        result = await base_processor.should_process_event(event)
+        assert result is False
+
+    @pytest.mark.asyncio
+    async def test_should_process_event_with_unsupported_hash(
+        self,
+        base_processor: BaseGitGuardianWebhookProcessor,
+        mock_context: PortOceanContext,
+    ) -> None:
+        self._set_integration_config(mock_context)
+
+        payload = {"action": "incident_triggered"}
+        body = json.dumps(payload).encode("utf-8")
+        signature = self._get_signature(body, "md5")
+        event = self._init_event(body, signature)
+
+        result = await base_processor.should_process_event(event)
+        assert result is False
+
+    @abstractmethod
+    async def test_get_matching_kinds(
+        self,
+        internal_incident_processor: InternalSecretIncidentWebhookProcessor,
+    ) -> None:
+        pass
+
+    def _get_signature(self, body: bytes, hash_algorithm: str):
+        return f"{hash_algorithm}={hmac.new(
+            bytes(TIMESTAMP + WEBHOOK_SECRET, "utf-8"), body, hashlib.sha256
+        ).hexdigest()}"
+
+    def _init_event(self, body: bytes, signature: str):
+        event = MagicMock()
+        event._original_request = MagicMock()
+        event._original_request.body = AsyncMock(return_value=body)
+        event.headers = {"gitguardian-signature": signature, "timestamp": TIMESTAMP}
+        return event
+
+    def _set_integration_config(self, mock_context: PortOceanContext) -> None:
+        mock_config = MagicMock()
+        mock_config.integration.config = {"webhook_secret": WEBHOOK_SECRET}
+        mock_context.config = mock_config
 
 
-@pytest.mark.asyncio
-async def test_should_process_event_invalid_signature(
-    internal_incident_processor: InternalSecretIncidentWebhookProcessor,
-    mock_context: PortOceanContext,
-) -> None:
-    set_integration_config(mock_context)
+class TestInternalSecretIncidentWebhookProcessor(TestBaseWebhookProcessor):
+    __test__ = True
 
-    payload = {"action": "incident_triggered"}
-    body = json.dumps(payload).encode("utf-8")
-    signature = "sha256=invalidsignature"
-    event = init_event(body, signature)
+    @pytest.mark.asyncio
+    async def test_should_process_event_valid_signature_and_timestamp(
+        self,
+        internal_incident_processor: InternalSecretIncidentWebhookProcessor,
+        mock_context: PortOceanContext,
+    ) -> None:
+        super().test_should_process_event_valid_signature_and_timestamp(
+            internal_incident_processor, mock_context
+        )
 
-    result = await internal_incident_processor.should_process_event(event)
-    assert result is False
+    @pytest.mark.asyncio
+    async def test_should_process_event_invalid_signature(
+        self,
+        internal_incident_processor: InternalSecretIncidentWebhookProcessor,
+        mock_context: PortOceanContext,
+    ) -> None:
+        super().test_should_process_event_invalid_signature(
+            internal_incident_processor, mock_context
+        )
 
+    @pytest.mark.asyncio
+    async def test_should_process_event_with_unsupported_hash(
+        self,
+        internal_incident_processor: InternalSecretIncidentWebhookProcessor,
+        mock_context: PortOceanContext,
+    ) -> None:
+        super().test_should_process_event_with_unsupported_hash(
+            internal_incident_processor, mock_context
+        )
 
-@pytest.mark.asyncio
-async def test_should_process_event_with_unsupported_hash(
-    internal_incident_processor: InternalSecretIncidentWebhookProcessor,
-    mock_context: PortOceanContext,
-) -> None:
-    set_integration_config(mock_context)
-
-    payload = {"action": "incident_triggered"}
-    body = json.dumps(payload).encode("utf-8")
-    signature = build_signature(body, "md5")
-    event = init_event(body, signature)
-
-    result = await internal_incident_processor.should_process_event(event)
-    assert result is False
-
-
-def build_signature(body, hash_algorithm):
-    return f"{hash_algorithm}={hmac.new(
-        bytes(TIMESTAMP + WEBHOOK_SECRET, "utf-8"), body, hashlib.sha256
-    ).hexdigest()}"
-
-
-def init_event(body, signature):
-    event = MagicMock()
-    event._original_request = MagicMock()
-    event._original_request.body = AsyncMock(return_value=body)
-    event.headers = {"gitguardian-signature": signature, "timestamp": TIMESTAMP}
-    return event
+    @pytest.mark.asyncio
+    async def test_get_matching_kinds(
+        self,
+        internal_incident_processor: InternalSecretIncidentWebhookProcessor,
+    ) -> None:
+        event = WebhookEvent(trace_id="test-trace-id", payload={}, headers={})
+        result = await internal_incident_processor.get_matching_kinds(event)
+        assert result == [ObjectKind.INTERNAL_SECRET_INCIDENT]
 
 
-def set_integration_config(mock_context) -> None:
-    mock_config = MagicMock()
-    mock_config.integration.config = {"webhook_secret": WEBHOOK_SECRET}
-    mock_context.config = mock_config
+class TestPublicSecretIncidentWebhookProcessor(TestBaseWebhookProcessor):
+    __test__ = True
+
+    @pytest.mark.asyncio
+    async def test_should_process_event_valid_signature_and_timestamp(
+        self,
+        public_incident_webhook_processor: PublicSecretIncidentWebhookProcessor,
+        mock_context: PortOceanContext,
+    ) -> None:
+        super().test_should_process_event_valid_signature_and_timestamp(
+            public_incident_webhook_processor, mock_context
+        )
+
+    @pytest.mark.asyncio
+    async def test_should_process_event_invalid_signature(
+        self,
+        public_incident_webhook_processor: PublicSecretIncidentWebhookProcessor,
+        mock_context: PortOceanContext,
+    ) -> None:
+        super().test_should_process_event_invalid_signature(
+            public_incident_webhook_processor, mock_context
+        )
+
+    @pytest.mark.asyncio
+    async def test_should_process_event_with_unsupported_hash(
+        self,
+        public_incident_webhook_processor: PublicSecretIncidentWebhookProcessor,
+        mock_context: PortOceanContext,
+    ) -> None:
+        super().test_should_process_event_with_unsupported_hash(
+            public_incident_webhook_processor, mock_context
+        )
+
+    @pytest.mark.asyncio
+    async def test_get_matching_kinds(
+        self,
+        public_incident_webhook_processor: PublicSecretIncidentWebhookProcessor,
+    ) -> None:
+        event = WebhookEvent(trace_id="test-trace-id", payload={}, headers={})
+        result = await public_incident_webhook_processor.get_matching_kinds(event)
+        assert result == [ObjectKind.PUBLIC_SECRET_INCIDENT]
